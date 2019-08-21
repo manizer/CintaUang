c43d52a23a081c3f6919333c6c8efa2435e5417c
Fernando Christyanto <christyantofernando@gmail.com>
2019-08-21 12:13:08 +0700

    Add Category Controller

---
+++ CintaUang/CintaUang/Controllers/CategoryControllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CintaUang.ViewModels.CategoryViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Base.Helper;
+using Repository.Repositories;
+
+namespace CintaUang.Controllers.CategoryControllers
+{
+    public class CategoryController : Controller
+    {
+		private readonly ICategoryRepository categoryRepository;
+		private readonly UnitOfWork unitOfWork;
+
+		public CategoryController(ICategoryRepository categoryRepository,
+			UnitOfWork unitOfWork)
+		{
+			this.categoryRepository = categoryRepository;
+			this.unitOfWork = unitOfWork;
+		}
+
+		public async Task<IActionResult> Index()
+        {
+			var categories = (await categoryRepository.GetCategories()).ToList();
+
+			IndexViewModel viewModel = new IndexViewModel
+			{
+				Categories = categories
+			};
+            return View(viewModel);
+        }
+    }
+}
---
+++ CintaUang/CintaUang/ViewModels/CategoryViewModels/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Model.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CintaUang.ViewModels.CategoryViewModels
+{
+	public class IndexViewModel
+	{
+		public List<Category> Categories { get; set; }
+	}
+}
